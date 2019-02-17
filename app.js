1c828d863a8156d220dc80244abdc21750aa4f1c
unknown <i.fermevc@gmail.com>
2019-02-17 19:19:01 +0100

    passport cookie working

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use(passport.session());
 
 //connect to DB
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+//set this because of some warning...
+mongoose.set("useCreateIndex", true);
 
 //create Schema
 const userSchema = new mongoose.Schema({
@@ -54,16 +56,53 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 app.get("/logout", (req, res) => {
+  req.logout();
   res.redirect("/");
 });
 app.get("/register", (req, res) => {
   res.render("register");
 });
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 //add register post route
-app.post("/register", (req, res) => {});
+app.post("/register", (req, res) => {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
 
 //add login post route
-app.post("/login", (req, res) => {});
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
 
 //start server
 app.listen(3000, () => {
