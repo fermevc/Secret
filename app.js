cc7da3131bbc5ded49cfd558ee2be85892d01718
unknown <i.fermevc@gmail.com>
2019-02-17 18:33:23 +0100

    i dont know what i'm doing :)

--- app.js
+++ app.js
@@ -3,24 +3,49 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = "passport-local-mongoose";
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//prepare express-session config
+app.use(
+  session({
+    secret: "Ourlittlesecret.",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+//initialize passport module
+app.use(passport.initialize());
+app.use(passport.session());
+
 //connect to DB
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
 //create Schema
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+//add passport plugin for mongoose
+userSchema.plugin(passportLocalMongoose);
+
 //create model using User schema
 const User = new mongoose.model("User", userSchema);
+
+//simplified passport-local config
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //routes setup
 app.get("/", (req, res) => {
   res.render("home");
@@ -29,52 +54,16 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 app.get("/logout", (req, res) => {
-  res.redirect("login");
+  res.redirect("/");
 });
 app.get("/register", (req, res) => {
   res.render("register");
 });
 //add register post route
-app.post("/register", (req, res) => {
-<<<<<<< HEAD
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-=======
-  bcrypt.hash(res.body.password, saltRounds, (err, hash) => {
->>>>>>> 50e8dc145dd7b481caffc68229daf0b0cd963ab4
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-    newUser.save(err => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
-});
+app.post("/register", (req, res) => {});
 
 //add login post route
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = md5(req.body.password);
-  User.findOne({ email: username }, (err, foundUser) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        } else {
-          res.render("login");
-        }
-      } else {
-        res.render("login");
-      }
-    }
-  });
-});
+app.post("/login", (req, res) => {});
 
 //start server
 app.listen(3000, () => {
